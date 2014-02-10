90aa1471e422e5e030b1b882dc52f25e55f43b8b
Jordan Ramey <jramey@qualtrax.com>
2014-02-09 20:03:20 -0500

    Changed variable name

--- KnightsTour.js/static/knightCommandManager.js
+++ KnightsTour.js/static/knightCommandManager.js
@@ -2,25 +2,25 @@
     this.undoStack = [];
     this.redoStack = [];
 
-    this.execute = function execute(command) {
-        command.execute();
+    this.execute = function execute(knightCommand) {
+        knightCommand.execute();
         this.undoStack.push(knightCommand);
         this.redoStack = [];
     }
 
     this.undo = function undo() {
         if (this.undoStack.length > 1) {
-            var command = this.undoStack.pop();
-            command.undo();
-            this.redoStack.push(command);
+            var knightCommand = this.undoStack.pop();
+            knightCommand.undo();
+            this.redoStack.push(knightCommand);
         }
     }
 
     this.redo = function redo() {
         if (this.redoStack.length > 0) {
-            var command = this.redoStack.pop();
-            command.redo();
-            this.undoStack.push(command);
+            var knightCommand = this.redoStack.pop();
+            knightCommand.redo();
+            this.undoStack.push(knightCommand);
         }
     }
 }
